633001bd2e2f4e82740331665dd839922bd35e13
paladin4ua <vkubiv@emp.org.ua>
2018-03-20 11:45:48 +0200

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var cuebiq;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,12 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  sendEvent() {
+    cuebiq.collectCustomEvents("eventName", "eventInfo1", "eventInfo2", "eventInfo3", "eventInfo4"
+        , () => console.log("Success")
+        , () => console.log("Fail"));
   }
 
 }
